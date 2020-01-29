0b117a09e8fe3857ebc4996d64acd4732a48f51b
Edzhevit <nightwalker_@abv.bg>
2020-01-29 10:37:10 +0200

    Added a new route

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/food_finder", {useNewUrlParser: true});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var restaurants = [
-    {
-        name: "Paris Eiffel Tower",
-        image: "https://i.pinimg.com/originals/69/10/20/691020528ff605e9ddef80ae22ea239a.jpg"
-    },
-    {
-        name: "Cave Groffa",
-        image: "https://bossluxury.files.wordpress.com/2014/04/10151772_859088060773502_892807270_n.jpg"
-    },
-    {
-        name: "The Griffi Palace",
-        image: "https://amp.businessinsider.com/images/55bfd5b62acae70f008bcb27-1136-852.jpg"
-    },
-    {
-        name: "Paris Eiffel Tower",
-        image: "https://i.pinimg.com/originals/69/10/20/691020528ff605e9ddef80ae22ea239a.jpg"
-    },
-    {
-        name: "Cave Groffa",
-        image: "https://bossluxury.files.wordpress.com/2014/04/10151772_859088060773502_892807270_n.jpg"
-    },
-    {
-        name: "The Griffi Palace",
-        image: "https://amp.businessinsider.com/images/55bfd5b62acae70f008bcb27-1136-852.jpg"
-    },
-    {
-        name: "Paris Eiffel Tower",
-        image: "https://i.pinimg.com/originals/69/10/20/691020528ff605e9ddef80ae22ea239a.jpg"
-    },
-    {
-        name: "Cave Groffa",
-        image: "https://bossluxury.files.wordpress.com/2014/04/10151772_859088060773502_892807270_n.jpg"
-    },
-    {
-        name: "The Griffi Palace",
-        image: "https://amp.businessinsider.com/images/55bfd5b62acae70f008bcb27-1136-852.jpg"
-    },
-];
+// SCHEMA SETUP
+var restaurantSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/restaurants", (req, res) => {
-
-    res.render("restaurants", {restaurants:restaurants});
+    // Get all restaurants from DB
+    Restaurant.find({}, (err, allRestaurants) => {
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", { restaurants: allRestaurants});
+        }
+    });
 });
 
-app.post("/restaurants", (req, res) =>{
+app.post("/restaurants", (req, res) => {
     // get data from form and add to restaurants array
     var name = req.body.name;
     var image = req.body.image;
-    var newRestaurant = {name: name, image: image};
-    restaurants.push(newRestaurant);
-    // redirect back to restaurants
-    res.redirect("/restaurants");
+    var description = req.body.description;
+    var newRestaurant = { name: name, image: image, description: description };
+    // create a new restaurant and save it to DB
+    Restaurant.create(newRestaurant, (err, newlyCreated) => {
+        if(err){
+            console.log(err);
+        } else{
+            // redirect back to restaurants
+            res.redirect("/restaurants");
+        }
+    });
 });
 
 app.get("/restaurants/new", (req, res) => {
-    res.render("new.ejs");
+    res.render("new");
+});
+
+app.get("/restaurants/:id", (req, res) => {
+    // find restaurant with provided ID
+    Restaurant.findById(req.params.id, (err, foundRestaurant) => {
+        if(err){
+            console.log(err)
+        } else{
+            // render show template with that restaurant
+            res.render("show", {restaurant: foundRestaurant})
+        }
+    });
+  
 });
 
 app.listen("3000", () => {
