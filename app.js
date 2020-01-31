8396582f38bf6bf451de09caccfadc91e1b991cb
Edzhevit <nightwalker_@abv.bg>
2020-01-31 10:15:15 +0200

    Added RESTful routes for comments

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Restaurant = require("./models/restaurant");
+var Comment = require("./models/comment");
+var seedDb = require("./seeds");
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost:27017/food_finder", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-// SCHEMA SETUP
-var restaurantSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Restaurant = mongoose.model("Restaurant", restaurantSchema);
+seedDb();
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -28,7 +25,7 @@ app.get("/restaurants", (req, res) => {
         if(err){
             console.log(err);
         } else{
-            res.render("index", { restaurants: allRestaurants});
+            res.render("restaurants/index", { restaurants: allRestaurants});
         }
     });
 });
@@ -51,20 +48,49 @@ app.post("/restaurants", (req, res) => {
 });
 
 app.get("/restaurants/new", (req, res) => {
-    res.render("new");
+    res.render("restaurants/new");
 });
 
 app.get("/restaurants/:id", (req, res) => {
     // find restaurant with provided ID
-    Restaurant.findById(req.params.id, (err, foundRestaurant) => {
+    Restaurant.findById(req.params.id).populate("comments").exec((err, foundRestaurant) => {
         if(err){
             console.log(err)
         } else{
             // render show template with that restaurant
-            res.render("show", {restaurant: foundRestaurant})
+            res.render("restaurants/show", {restaurant: foundRestaurant});
         }
     });
   
+});
+
+app.get("/restaurants/:id/comments/new", (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) =>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render("comments/new", {restaurant: restaurant})
+        }
+    })
+});
+
+app.post("/restaurants/:id/comments", (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) =>{
+        if(err){
+            console.log(err);
+            req.redirect("/restaurants")
+        } else{
+            Comment.create(req.body.comment, (err, comment) => {
+                if(err){
+                    console.log(err);
+                } else{
+                    restaurant.comments.push(comment);
+                    restaurant.save();
+                    res.redirect("/restaurants/" + restaurant.id);
+                }
+            })
+        }
+    })
 });
 
 app.listen("3000", () => {
