a024acad3a8381b31dad4619beacdda6e8e8b24d
Edzhevit <nightwalker_@abv.bg>
2020-02-03 10:33:29 +0200

    Required flash-connector

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var flash = require("connect-flash");
 var passport = require("passport");
 var LogalStrategy = require("passport-local");
 var methodOverride = require("method-override");
@@ -15,14 +16,14 @@ var restaurantRoutes = require("./routes/restaurants");
 var commentRoutes = require("./routes/comments");
 
 mongoose.set('useUnifiedTopology', true);
-mongoose.set('useFindAndModify', false);
-mongoose.connect("mongodb://localhost:27017/food_finder", {useNewUrlParser: true});
+// mongoose.set('useFindAndModify', false);
+mongoose.connect("mongodb://localhost:27017/food_finder2", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-
+app.use(flash());
 // seedDb();
 
 app.use(expressSession({
@@ -38,6 +39,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
