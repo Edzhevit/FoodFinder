168236e9e115710fcd34c4d3f1a6cf40a5863560
Edzhevit <nightwalker_@abv.bg>
2020-02-10 10:27:45 +0200

    Add slug object to place model so instead of id it show a slug on query

--- models/place.js
+++ models/place.js
@@ -1,7 +1,10 @@
 var mongoose = require("mongoose");
 
 var placeSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: "Place name cannot be blank."
+    },
     image: String,
     description: String,
     location: String,
@@ -33,7 +36,61 @@ var placeSchema = new mongoose.Schema({
     rating: {
         type: Number,
         default: 0
+    },
+    slug: {
+        type: String,
+        unique: true
     }
 });
 
-module.exports = mongoose.model("Place", placeSchema);
+// add a slug before the place gets saved to the DB
+placeSchema.pre("save", async function (next) {
+   try {
+       // check if new place is being saved, or if place is being modified
+       if (this.isNew || this.isModified("name")){
+           this.slug = await generateUniqueSlug(this.id, this.name);
+       }
+       next();
+   } catch (err) {
+       next(err);
+   }
+});
+
+var Place = mongoose.model("Place", placeSchema);
+
+module.exports = Place;
+
+async function generateUniqueSlug (id, placeName, slug) {
+    try {
+        // generate initial slug
+        if (!slug){
+            slug = slugify(placeName);
+        }
+
+        // check if place with that slug already exists
+        var place = await Place.findOne({slug: slug});
+        // check if the place is found or if the found place is the current place
+        if (!place || place.id.equal(id)){
+            return slug;
+        }
+
+        // if not unique generate a new slug
+        var newSlug = slugify(placeName);
+
+        // check again by calling the function recursively
+        return await generateUniqueSlug(id, placeName, newSlug);
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+function slugify(text) {
+    var slug = text.toString().toLowerCase()
+        .replace(/\s+/g, '-')        // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')    // Remove all non-word chars
+        .replace(/\-\-+/g, '-')      // Replace multiple - with single -
+        .replace(/^-+/, '')          // Trim - from start of text
+        .replace(/-+$/, '')          // Trim - from end of text
+        .substring(0, 75);           // Trim at 75 characters
+    return slug + "-" + Math.floor(1000 + Math.random() * 9000);  // Add 4 random digits to improve uniqueness
+}
