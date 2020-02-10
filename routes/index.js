9746f89b31944674763c99650bbb646b1baa7758
Edzhevit <nightwalker_@abv.bg>
2020-02-10 11:04:36 +0200

    Last fixes

--- routes/index.js
+++ routes/index.js
@@ -69,130 +69,130 @@ router.get("/logout", (req, res) => {
 
 // To use the forgot and reset you need to change your gmail settings so it allows less secure apps
 // get forgot password form
-router.get("/forgot", (req, res) => {
-    res.render("forgot");
-});
+// router.get("/forgot", (req, res) => {
+//     res.render("forgot");
+// });
 
 // To use the forgot and reset you need to change your gmail settings so it allows less secure apps
 // handle forgot password logic
-router.post("/forgot", (req, res, next) => {
-    async.waterfall([
-        function (done) {
-            crypto.randomBytes(20, (err, buf) => {
-                var token = buf.toString("hex");
-                done(err, token);
-            });
-        },
-        function (token, done) {
-            User.findOne({email: req.body.email}, (err, foundUser) => {
-                if(!foundUser){
-                    req.flash("error", "No account with that email address exists!");
-                    return res.redirect("/forgot");
-                }
-                foundUser.resetPasswordToken = token;
-                foundUser.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-                foundUser.save((err) => {
-                    done(err, token, foundUser);
-                });
-            });
-        },
-        function (token, user, done) {
-            var smtpTransport = nodeMailer.createTransport({
-                service: "Gmail",
-                auth: {
-                    // You need to put your own email and password here
-                    user: "test@gmail.com",
-                    pass: "test"
-                }
-            });
-            var mailOptions = {
-                to: user.email,
-                // You need to put your own email here
-                from: "test@gmail.com",
-                subject: "PlaceFinder password reset",
-                text: "You are receiving this email because you (or someone else) have requested reset of the password for your account." +
-                      "Please click on the following link, or paste this into your browser to complete the process:" +
-                      "http://" + req.headers.host + "/reset/" + token + "\n\n" +
-                      "If you did not request this, please ignore this email and your password will remain unchanged."
-            };
-            smtpTransport.sendMail(mailOptions, (err) => {
-                req.flash("success", "An email has been sent to " + user.email + " with further instructions.");
-                done(err, "done");
-            });
-        }
-    ], (err) => {
-        if (err){
-            return next(err);
-        }
-        res.redirect("forgot")
-    });
-});
+// router.post("/forgot", (req, res, next) => {
+//     async.waterfall([
+//         function (done) {
+//             crypto.randomBytes(20, (err, buf) => {
+//                 var token = buf.toString("hex");
+//                 done(err, token);
+//             });
+//         },
+//         function (token, done) {
+//             User.findOne({email: req.body.email}, (err, foundUser) => {
+//                 if(!foundUser){
+//                     req.flash("error", "No account with that email address exists!");
+//                     return res.redirect("/forgot");
+//                 }
+//                 foundUser.resetPasswordToken = token;
+//                 foundUser.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+//
+//                 foundUser.save((err) => {
+//                     done(err, token, foundUser);
+//                 });
+//             });
+//         },
+//         function (token, user, done) {
+//             var smtpTransport = nodeMailer.createTransport({
+//                 service: "Gmail",
+//                 auth: {
+//                     // You need to put your own email and password here
+//                     user: "test@gmail.com",
+//                     pass: "test"
+//                 }
+//             });
+//             var mailOptions = {
+//                 to: user.email,
+//                 // You need to put your own email here
+//                 from: "test@gmail.com",
+//                 subject: "PlaceFinder password reset",
+//                 text: "You are receiving this email because you (or someone else) have requested reset of the password for your account." +
+//                       "Please click on the following link, or paste this into your browser to complete the process:" +
+//                       "http://" + req.headers.host + "/reset/" + token + "\n\n" +
+//                       "If you did not request this, please ignore this email and your password will remain unchanged."
+//             };
+//             smtpTransport.sendMail(mailOptions, (err) => {
+//                 req.flash("success", "An email has been sent to " + user.email + " with further instructions.");
+//                 done(err, "done");
+//             });
+//         }
+//     ], (err) => {
+//         if (err){
+//             return next(err);
+//         }
+//         res.redirect("forgot")
+//     });
+// });
 
 // To use the forgot and reset you need to change your gmail settings so it allows less secure apps
 // get reset password form
-router.get("/reset/:token", (req, res) => {
-    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, (err, foundUser) => {
-        if (!foundUser){
-            req.flash("error", "Password reset token is invalid or has expired!");
-            return res.redirect("/forgot");
-        }
-        res.render("reset", {token: req.params.token});
-    });
-});
+// router.get("/reset/:token", (req, res) => {
+//     User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, (err, foundUser) => {
+//         if (!foundUser){
+//             req.flash("error", "Password reset token is invalid or has expired!");
+//             return res.redirect("/forgot");
+//         }
+//         res.render("reset", {token: req.params.token});
+//     });
+// });
 
 // To use the forgot and reset you need to change your gmail settings so it allows less secure apps
 // handle reset password logic
-router.post("/reset/:token", (req, res) => {
-    async.waterfall([
-        function (done) {
-            User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, (err, user) => {
-                if (!user){
-                    req.flash("error", "Password reset token is invalid or has expired!");
-                    return res.redirect("back");
-                }
-                if (req.body.password === req.body.confirmPassword){
-                    user.setPassword(req.body.password, (err) => {
-                        user.resetPasswordToken = undefined;
-                        user.resetPasswordExpires = undefined;
-                        user.save((err) => {
-                            req.logiIn(user, (err) => {
-                                done(err, user);
-                            });
-                        });
-                    })
-                } else {
-                    req.flash("error", "Passwords do not match.");
-                    return res.redirect("back");
-                }
-            });
-        },
-        function (user, done) {
-            var smtpTransport = nodeMailer.createTransport({
-                service: "Gmail",
-                auth: {
-                    // You need to put your own email and password here
-                    user: "test@gmail.com",
-                    pass: "test"
-                }
-            });
-            var mailOptions = {
-                to: user.email,
-                // You need to put your own email here
-                from: "test@gmail.com",
-                subject: "Your password has been changed!",
-                text: "Hello,\n\n" +
-                      "This is a confirmation that the password for your account " + user.email + " has just changed."
-            };
-            smtpTransport.sendMail(mailOptions, (err) => {
-                req.flash("success", "Your password has been changed!");
-                done(err);
-            });
-        }
-    ], (err) => {
-        res.redirect("/places");
-    });
-});
+// router.post("/reset/:token", (req, res) => {
+//     async.waterfall([
+//         function (done) {
+//             User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, (err, user) => {
+//                 if (!user){
+//                     req.flash("error", "Password reset token is invalid or has expired!");
+//                     return res.redirect("back");
+//                 }
+//                 if (req.body.password === req.body.confirmPassword){
+//                     user.setPassword(req.body.password, (err) => {
+//                         user.resetPasswordToken = undefined;
+//                         user.resetPasswordExpires = undefined;
+//                         user.save((err) => {
+//                             req.logiIn(user, (err) => {
+//                                 done(err, user);
+//                             });
+//                         });
+//                     })
+//                 } else {
+//                     req.flash("error", "Passwords do not match.");
+//                     return res.redirect("back");
+//                 }
+//             });
+//         },
+//         function (user, done) {
+//             var smtpTransport = nodeMailer.createTransport({
+//                 service: "Gmail",
+//                 auth: {
+//                     // You need to put your own email and password here
+//                     user: "test@gmail.com",
+//                     pass: "test"
+//                 }
+//             });
+//             var mailOptions = {
+//                 to: user.email,
+//                 // You need to put your own email here
+//                 from: "test@gmail.com",
+//                 subject: "Your password has been changed!",
+//                 text: "Hello,\n\n" +
+//                       "This is a confirmation that the password for your account " + user.email + " has just changed."
+//             };
+//             smtpTransport.sendMail(mailOptions, (err) => {
+//                 req.flash("success", "Your password has been changed!");
+//                 done(err);
+//             });
+//         }
+//     ], (err) => {
+//         res.redirect("/places");
+//     });
+// });
 
 // user profile
 router.get("/users/:id", async (req, res) => {
@@ -203,20 +203,6 @@ router.get("/users/:id", async (req, res) => {
         req.flash("error", err.message);
         return res.redirect("back");
     }
-    // User.findById(req.params.id, (err, foundUser) => {
-    //     if (err){
-    //         req.flash("error", "There is no such user!");
-    //         res.redirect("back");
-    //     }
-    //     Place.find().where("user.id").equals(foundUser.id).exec((err, places) => {
-    //         if (err){
-    //             req.flash("error", "There is no such user!");
-    //             res.redirect("back");
-    //         }
-    //         res.render("users/show", {user: foundUser, places: places});
-    //     });
-    //
-    // });
 });
 
 // follow user
